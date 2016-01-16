d3ddeddc2d5f7956a7a2aebf6ad98e5926b5bafc
tpphu <vietean@gmail.com>
2016-01-16 18:03:46 +0700

    Add migratation db

---
+++ database/migrations/2016_01_16_105327_create_customers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->tinyInteger('gender');
+            $table->date('dob');
+            $table->string('email');
+            $table->string('mobile_phone');
+            $table->string('image');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('customers');
+    }
+}
